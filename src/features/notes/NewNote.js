f9017a6c2e200dc29594c973ddd5eb6a94db3adc
Senthur Kumaran <senthur@Senthurs-MBP.lan>
2022-09-04 10:39:19 +0530

    tech notes front end work

---
+++ src/features/notes/NewNote.js
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+import { selectAllUsers } from '../users/usersApiSlice';
+import NewNoteForm from './NewNoteForm';
+
+const NewNote = () => {
+  const users = useSelector(selectAllUsers);
+
+  if (!users?.length) return <p>Not Currently Available</p>;
+
+  return <NewNoteForm users={users} />;
+};
+
+export default NewNote;
